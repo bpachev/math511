24468d685efbb8246c29ceedf1f1015cb5517769
Benjamin Pachev <benjaminpachev@gmail.com>
2020-02-05 16:39:38 -0700

    Ported the stable finite difference coefficient code. Finished the code for Problem 1 on HW4.

--- fdutil.py
+++ fdutil.py
@@ -4,13 +4,54 @@ from numpy.linalg import solve
 A utility file containing routines for generating finite difference coefficients.
 """
 
-def FDCoeffs(xb, x, k, ret_error = False):
+#Here is the MATLAB code from the book, translated to Python
+
+def stable_fdcoeffs(xbar, x, k):
+	n = len(x)
+	if k >= n:
+		error('*** length(x) must be larger than k')
+
+	m = k;   # change to m=n-1 if you want to compute coefficients for all
+	         # possible derivatives.  Then modify to output all of C.
+	#Add padding so the 1-based indexing actually works
+	x_new = np.zeros(len(x)+1)
+	x_new[1:] = x
+	x = x_new
+	c1 = 1;
+	c4 = x[1] - xbar
+	#Add an extra row and column so that the 1-based indexing doesn't break
+	C = np.zeros((n-1+1+1,m+1+1))
+	C[1,1] = 1
+	for i in range(1,n):
+		i1 = i+1
+		mn = min(i,m)
+		c2 = 1
+		c5 = c4
+		c4 = x[i1] - xbar
+		for j in range(0,i):
+			j1 = j+1
+			c3 = x[i1] - x[j1]
+			c2 = c2*c3
+			if j==i-1:
+				for s in list(range(1,mn+1))[::-1]:
+					s1 = s+1
+					C[i1,s1] = c1*(s*C[i1-1,s1-1] - c5*C[i1-1,s1])/c2
+				C[i1,1] = -c1*c5*C[i1-1,1]/c2
+			for s in list(range(1,mn+1))[::-1]:
+				s1 = s+1
+				C[j1,s1] = (c4*C[j1,s1] - s*C[j1,s1-1])/c3
+			C[j1,1] = c4*C[j1,1]/c3
+		c1 = c2
+	return C[1:,-1]
+
+def FDCoeffs(xb, x, k, ret_error = False, stable=False):
 	"""
 	Input:
 		xb -- a real number, the point at which to approximate the derivative
 		x -- a vector of grid points. Must have length >= k+1
 		k -- an integer, the order of derivative being approximated
 		ret_error -- boolean
+		stable -- whether to avoid underflow
 	Returns:
 		c -- the coefficients of the finite difference
 		error (optional) -- the coefficient by the next order derivative, or the truncation error
@@ -23,13 +64,20 @@ def FDCoeffs(xb, x, k, ret_error = False):
 	b[k] = 1
 	denom = 1.
 	for j in range(n):
-		A[j,:] = (x-xb)**j/denom
+		if not stable or j <= k: A[j,:] = (x-xb)**j/denom
+		if stable and j > k:
+			A[j] = A[j-1] * (x-xb)
+			A[j] /= np.sqrt(np.sum(A[j]**2))
 		denom *= (j+1)
-	
-	c = solve(A,b)
+	try:
+		c = solve(A,b)
+	except:
+		print stable_fdcoeffs(xb, x, k), stable_fdcoeffs(1, np.arange(0,3), 2)
+#		print A, A.shape, xb, x-xb
+		raise
 	err0 = np.dot(c,A[-1]*(x-xb)/n)
 	err1 = np.dot(c,A[-1]*(x-xb)**2/n/(n+1))
-	return c, err0,err1 if ret_error else c
+	return (c, err0,err1) if ret_error else c
 
 def FDStencil(k, j, verbose=True):
 	"""
---
+++ hw4.py
@@ -0,0 +1,91 @@
+import numpy as np
+import fdutil as fd
+from scipy.linalg import solve_banded
+
+def gen_grid(a,b,m,grid_type='uniform'):
+	dom = np.linspace(0,1,m+1)
+	if grid_type == 'uniform': return (b-a)*dom + a
+	elif grid_type == 'rtlayer': return (a-b)*(dom-1)**2 + b
+	elif grid_type == 'chebyshev': return -(b-a)/2. * np.cos(np.pi*dom) + (b+a)/2.
+	else: raise ValueError("Unrecognized grid type {}".format(grid_type))
+
+#Code for solving the boundary value problem epsu''-u = f(x) to second order, with a nonuniform grid
+def prob1_bvp(f, grid, alpha=1, beta=3, eps=1e-2):
+	#Assumes that grid[0] and grid[-1] are where the boundary conditions apply, and that the boundary conditions are Dirichlet
+	n = len(grid) - 2 #number of unkowns
+	ab = np.zeros((3,n)) #The matrix to store diagonals for scipy.solve_banded
+	b = f(grid)[1:-1]
+	for i in range(1,n+1):
+		second_coeffs = fd.FDCoeffs(grid[i], grid[i-1:i+2],2)
+		first_coeffs = fd.FDCoeffs(grid[i], grid[i-1:i+2],1)
+#		print(second_coeffs, first_coeffs)
+		coeffs = eps*second_coeffs - first_coeffs
+		#main diagonal
+		ab[1,i-1] = coeffs[1]
+		#Upper diagonal
+		if i < n: ab[0,i] = coeffs[2]
+		else: b[-1] -= coeffs[2]*beta
+		#Lower diagonal
+		if i > 1: ab[2,i-2] = coeffs[0]
+		else: b[0] -= coeffs[0]*alpha
+	
+	res = np.zeros(len(grid))
+	res[0] = alpha
+	res[-1] = beta
+	res[1:-1] = solve_banded((1,1), ab, b)	
+	return res
+
+def l2_norm(dom, y):
+	heights = (y[1:]**2+y[:-1]**2)/2.
+	return np.sqrt(np.dot(heights, np.diff(dom)))
+
+def make_table_data(u, f, sol_func,mvals):
+	errs = []
+	hvals = []
+	orders = [0]
+	for i,m in enumerate(mvals):
+		grid, approx = sol_func(f,m)
+		errs.append(l2_norm(grid, u(grid)-approx))
+		hvals.append(1./m)
+		if i: orders.append(np.polyfit(np.log(np.array(hvals)),np.log(np.array(errs)),1)[0])
+	return [hvals, errs, orders]
+
+from numpy.linalg import solve
+
+def prob1_pseudospec(f,grid,alpha=1, beta=3, eps=1e-2):
+	n = len(grid) - 2
+	A = np.zeros((n,n))
+	sol = f(grid)
+	sol[0] = alpha
+	sol[-1] = beta
+	b = f(grid[1:-1])
+	for i in range(1,n+1):
+		second_coeffs = fd.stable_fdcoeffs(grid[i], grid, 2)
+		first_coeffs = fd.stable_fdcoeffs(grid[i], grid, 1)
+		coeffs = eps*second_coeffs - first_coeffs
+		A[i-1] = coeffs[1:-1]
+		b[i-1] -= alpha * coeffs[0] + beta * coeffs[-1]
+	sol[1:-1] = solve(A,b)
+	return sol
+
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+	f = lambda x: x-1 #Forcing function for problem 1
+	eps = 1e-2
+	u1 = lambda x: (1.-np.exp(x/eps))/(1-np.exp(1./eps)) * (3./2+eps) - 1./2*x**2 + (1-eps)*x + 1
+	for t in ['uniform','rtlayer','chebyshev']:
+#		g = gen_grid(0,1,40, t)
+#		sol = prob1_pseudospec(f,g, eps=eps)
+#		plt.plot(g, sol, label=t)
+#		if t == 'chebyshev':
+#			plt.plot(g, u1(g), label='true')
+#		continue
+		if t != 'chebyshev': continue
+		def sol_func(f,m):
+			g = gen_grid(0,1,m, t)
+			sol = prob1_pseudospec(f,g, eps=eps)
+			return g, sol
+		print make_table_data(u1, f, sol_func, list(range(25,60,5)))
+#	plt.legend()
+#	plt.show()
