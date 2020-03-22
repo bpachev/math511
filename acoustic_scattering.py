cc13da612090e45690e9df0c79d6bab8c186f12c
Benjamin Pachev <benjaminpachev@gmail.com>
2020-03-21 22:38:40 -0600

    Have the computation of the exact solution working, as well as the finite difference for one term of the Farfield Expansion.

--- acoustic_scattering.py
+++ acoustic_scattering.py
@@ -5,27 +5,29 @@ from fdutil import FDStencil
 from scipy.special import jn, hankel1 #Bessel and hankel functions
 
 def exact_soln(r, theta, terms = 30, r0=1, k = 2*np.pi):
-	sln = np.zeros_like(theta).astype(np.complex128)
+	r = np.array(r)
+	theta = np.array(theta)
+	sln = np.zeros((len(r), len(theta)), dtype=np.complex128)
 	for n in xrange(terms):
 		eps = 2 if n else 1
-		sln += 1j**n * jn(n,k*r0)/hankel1(n,k*r0)*hankel1(n,k*r)*np.cos(n*theta)
+		sln += eps* 1j**n * np.outer(jn(n,k*r0)/hankel1(n,k*r0)*hankel1(n,k*r), np.cos(n*theta))
 	return -sln
 
 def solve_acoustic(r0=1,R=2,PPW=30,k=2*np.pi, sparse = False):
+	theta_h = 2*np.pi/(PPW)
+	r_h = (R-r0)/float(PPW)
 	bcs = np.zeros(PPW, dtype=np.complex128)
 	thetas = np.linspace(0,2*np.pi,PPW+1)[:-1]
-	bcs[:] = -np.exp(1j*r0*np.cos(thetas))
+	bcs[:] = -np.exp(1j*k*r0*np.cos(thetas))
 	#So first we need to determine the number of unkowns
 	#This is going to be PPW * num distinct r
 	#If we have PPW-1 interior radial points, then we end up with PPW^2 unknowns, provided we use only one term in the farfield expansion
-	coeffs = FDStencil(1, np.array([-2,-1,0]), verbose=False).astype(np.complex128)
+	coeffs = FDStencil(1, np.array([-2,-1,0]), verbose=False).astype(np.complex128) / r_h
 	#This gives us the coeffs for the equation a*U(R-2h, theta) + b*U(R-h,theta) - c * U(R, theta) = 0
 	#which is what the ABC from the farfield expansion boils down to
 	#Using more than one term will require us to treat the f_k explicitly, but in this case we don't have to
 	coeffs[-1] -= 1j * k - 1./(2*R)
 	n = PPW**2
-	theta_h = 2*np.pi/PPW
-	r_h = (R-r0)/float(PPW)
 	if sparse:
 		pass
 	else:
@@ -38,17 +40,18 @@ def solve_acoustic(r0=1,R=2,PPW=30,k=2*np.pi, sparse = False):
 		#Add discrete polar Laplacian
 		rs = np.linspace(r0,R,PPW+1)
 		for i in xrange(PPW-1):
+			cur_r = rs[i+1]
 			for j in xrange(PPW):
 				#Add in the theta derivative
 				j_prev = (j-1+PPW) % PPW
 				j_next = (j+1) % PPW
 				pos = conv(i,j)
-				A[pos,pos] += k**2 - 1./theta_h**2 - 1./r_h**2
+				A[pos,pos] += k**2 - 2./theta_h**2/cur_r**2 - 2./r_h**2
 				#Now handle the r derivatives
-				A[pos,conv(i,j_prev)] += .5/theta_h**2
-				A[pos,conv(i,j_next)] += .5/theta_h**2
-				A[pos,conv(i+1, j)] += .5/r_h**2 + .5/r_h * 1./rs[i+1]
-				c = .5/r_h**2 - .5/r_h * 1./rs[i+1]
+				A[pos,conv(i,j_prev)] += 1/theta_h**2/cur_r**2
+				A[pos,conv(i,j_next)] += 1/theta_h**2/cur_r**2
+				A[pos,conv(i+1, j)] += 1/r_h**2 + .5/r_h / cur_r
+				c = 1/r_h**2 - .5/r_h / cur_r
 				if i:
 					A[pos, conv(i-1,j)] += c
 				else:
@@ -66,13 +69,31 @@ def solve_acoustic(r0=1,R=2,PPW=30,k=2*np.pi, sparse = False):
 		true_soln[PPW:] = solve(A,b)
 		return true_soln, rs, thetas
 
-PPW = 45
-R=5.
+def polar_to_cartesian(rs, thetas):
+	return np.outer(rs, np.cos(thetas)), np.outer(rs, np.sin(thetas))
+
+
+PPW = 40
+R=2.
+k = 2*np.pi
 res,rs,thetas = solve_acoustic(PPW=PPW,R=R)
 
-from matplotlib import pyplot as plt
+X,Y = polar_to_cartesian(rs,thetas)
+#print X,Y
+exact = exact_soln(rs, thetas, terms=30)
+u_inc = np.exp(1j*k*X)
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+hf = plt.figure()
+ha = hf.add_subplot(121, projection='3d')
+ha.plot_surface(X,Y, np.abs(u_inc+res.reshape(Y.shape)))
+ha = hf.add_subplot(122, projection='3d')
+ha.plot_surface(X,Y, np.abs(u_inc+exact))
+plt.show()
+
 plt.plot(thetas, np.abs(res[-PPW:]), label="Computed")
-plt.plot(thetas, np.abs(exact_soln(R, thetas,terms=30)), label="Exact")
+plt.plot(thetas, np.abs(exact[-1]), label="Exact")
 plt.legend()
 plt.show()
 
