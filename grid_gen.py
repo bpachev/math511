896f96dda2c349ac9536bd5ed3f2c04b58908621
Benjamin Pachev <benjaminpachev@gmail.com>
2020-04-17 21:35:18 -0600

    Have support for multiply connected domains.

--- grid_gen.py
+++ grid_gen.py
@@ -5,7 +5,7 @@ from matplotlib.colors import ListedColormap
 #from mayavi import mlab
 
 class gridGen():
-	def __init__(self, x,y, branch_cut=None, omega=1., tol=1e-5, max_its=5000):
+	def __init__(self, x,y, branch_cut=None, omega=1., tol=1e-5, max_its=5000, multiply_connected=False):
 		"""Set up a grid generation problem, where x and y contain the initial guess to the coordinates
 
 		The shape of x and y must be the same, and the boundaries must be correct for the grid generation to work.
@@ -17,6 +17,7 @@ class gridGen():
 		self.tol = tol
 		self.omega = omega
 		self.max_its = max_its
+		self.mc = multiply_connected
 		if x.shape != y.shape:
 			raise ValueError("x and y must have the same shape!")
 
@@ -52,6 +53,20 @@ class gridGen():
 		n1,n2 = self.x.shape
 
 		alpha, beta, gamma = self.winslow_coeffs()
+
+		#Handle the multiply connected case with a hole in the center
+		if self.mc:
+			x_xi = ((self.x[1] - self.x[-2])/2.)[1:-1]
+			x_eta = (self.x[0,2:] - self.x[0,:-2])/2.
+			y_xi = ((self.x[1] - self.y[-2])/2.)[1:-1]
+			y_eta = (self.y[0,2:] - self.y[0,:-2])/2.
+			a, b, g = x_eta**2+y_eta**2, x_eta*x_xi+y_eta*y_xi, x_xi**2 + y_xi**2
+			for j in range(1,n2-1):
+				x_hat = (a[j-1] * (self.x[-2,j]+self.x[1,j]) +g[j-1]*(self.x[0,j-1]+self.x[0,j+1]) -.5 * b[j-1] * (self.x[-2,j-1]-self.x[1,j-1]-self.x[-2,j+1]+self.x[1,j+1])) / (2 * (a[j-1]+g[j-1]))
+				y_hat = (a[j-1] * (self.y[-2,j]+self.y[1,j]) +g[j-1]*(self.y[0,j-1]+self.y[0,j+1]) -.5 * b[j-1] * (self.y[-2,j-1]-self.y[1,j-1]-self.y[-2,j+1]+self.y[1,j+1])) / (2 * (a[j-1]+g[j-1]))
+				self.x[0,j] = self.omega * x_hat + (1-self.omega)*self.x[0,j]
+				self.y[0,j] = self.omega * y_hat + (1-self.omega)*self.y[0,j]
+
 		for i in range(1,n1-1):
 			for j in range(1,n2-1):
 				a = alpha[i-1,j-1]
@@ -62,6 +77,9 @@ class gridGen():
 				self.x[i,j] = self.omega * x_hat + (1-self.omega)*self.x[i,j]
 				self.y[i,j] = self.omega * y_hat + (1-self.omega)*self.y[i,j]
 
+		if self.mc:
+			self.x[-1,:] = self.x[0]
+			self.y[-1,:] = self.y[0]
 		return self.check_convergence(old_x,old_y)
 		
 
@@ -122,9 +140,36 @@ def make_dome(n1,n2):
 	y[-1] = 2. - 4*(x_dom-.5)**2
 	return x,y
 
+def make_rose(n1,n2):
+	t = np.linspace(0,2*np.pi,n1)
+	x = np.zeros((n1,n2))
+	y = np.zeros((n1,n2))
+	r = .3*(2+np.cos(3*t))
+	R = 2
+	rs = r.reshape((n1,1)) + np.outer((R-r),np.linspace(0,1,n2))
+	x = rs * np.cos(t).reshape((n1,1))
+	y = rs * np.sin(t).reshape((n1,1))
+	return x,y
+
+def make_asteroid(n1,n2):
+	t = np.linspace(0,2*np.pi,n1)
+	x = np.zeros((n1,n2))
+	y = np.zeros((n1,n2))
+	x[:,0] = .5*(3*np.cos(t)+np.cos(3*t))
+	y[:,0] = .5*(3*np.sin(t)- np.sin(3*t))
+	R = 6
+	x[:,-1] = R*np.cos(t)
+	y[:,-1] = R*np.sin(t)
+	lambdas = np.linspace(0,1,n2)[1:-1]
+	for i in range(n1):
+		x[i,1:-1] = x[i,0] + lambdas*(x[i,-1]-x[i,0])
+		y[i,1:-1] = y[i,0] + lambdas*(y[i,-1]-y[i,0])
+
+	return x,y
+
 if __name__ == "__main__":
 	print(4/(2+(4-(np.cos(np.pi/41.)+np.cos(np.pi/41.))**2)**.5))
-	gen = gridGen(*make_swan(41,61), omega=1.5)
+	gen = gridGen(*make_asteroid(41,21), omega=1.5, multiply_connected=True)
 	gen.plot()
 	print(gen.solve(algo='Winslow'))
 	gen.plot()
